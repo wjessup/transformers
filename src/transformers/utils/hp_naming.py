264e4b82d66b4129575e7b068a40b67a842385f6
Will Jessup <will@willjessup.com>
2023-03-28 10:03:16 -0400

    Explanation:
    - First of all, we import the necessary modules and create a class for the refactoring process.
    - Then, we declare PREFIX and DEFAULTS, with a set of methods: set_defaults, shortname_for_word, shortname_for_key, add_new_param_name, build_naming_info, shortname_from_params, and parse_repr. 
    - In set_defaults, we update the prefix and default, and then call build_naming_info inside the static function.
    - In the shortname_for_word method, we first check if the passed `word` is an empty string. If so, we return an empty string directly. Next, if the word contains digits, we raise an exception. We then check if the word is present in the dictionary of `short_word`. 
    - If it is not, we loop through the `word` trying each prefix of increasing length until we get an unused prefix. If we reach the end of the loop without getting an unused prefix, we then create `para_chash` by iterating `int_to_alphabetic` over i, until para_chash is unique.
    - We then update the `short_word`, `reverse_short_word`, and return `short_word` for use.
    - The shortname_for_key method is used to create a key that is unique, given the input word. 
    - This is achieved by splitting the word based on '_' and calling `shortname_for_word`. The returned string is then joined with either an empty string or an underscore. If this resultant string is unique, it's set to param_name and returned.
    - The add_new_param_name method is used to call shortname for a new parameter name.
    - Concretely, i iterate over each character of `param_name` and call shortname on each word formed. I then update the `short_param` and `reverse_short_param` with the new pair.
    - The build_naming_info method is a helper static method that will be called anytime the code needs to be evaluated. It initializes `cls.NAMING_INFO` as a dictionary containing:
      - A dictionary with keys `short_word`, `reverse_short_word`, `short_param`, `reverse_short_param`.
      - `short_word` maps a string to a unique short word abbreviation. 
      - `reverse_short_word` maps a unique short word abbreviation to a string.
      - `short_param` maps a string to a unique abbreviation for that parameter.
      - `reverse_short_param` maps an abbreviation to the original string.
    - The shortname method generates a unique name to represent the input parameters by iterating over all the key value pairs of the input parameters and calling shortname_for_key method. 
    - Has `param_name` that we get from calling `shortname_for_key` for each key in `params`. If the value of the key is equal to the default value, then the name is not added to the final list to make the resultant name as concise as possible.
    - Finally, the parse_repr method accepts a string and process it to return a dictionary. We first remove the `hp_` identifier in the start of the string. If the string is empty, we initialize `values` to an empty list else we split the string by the underscore delimiter. 
    - We then iterate through each value in values and set `p_k` to the split string left substring and `p_v` to the split string right substring converted to float using `re.sub` for values with no dashes, and the second half of the split string right substring is converted to a float using `re.sub` for values containing the '-'. The last two statement of the function update the `parameters` with defaults and return the resultant dictionary.

--- src/transformers/utils/hp_naming.py
+++ src/transformers/utils/hp_naming.py
@@ -1,67 +1,45 @@
-# Copyright 2020 The HuggingFace Team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import copy
 import re
-
 
 class TrialShortNamer:
     PREFIX = "hp"
     DEFAULTS = {}
-    NAMING_INFO = None
 
-    @classmethod
-    def set_defaults(cls, prefix, defaults):
-        cls.PREFIX = prefix
-        cls.DEFAULTS = defaults
-        cls.build_naming_info()
+    @staticmethod
+    def set_defaults(prefix, defaults):
+        TrialShortNamer.PREFIX, TrialShortNamer.DEFAULTS = prefix, defaults
+        TrialShortNamer.build_naming_info()
+
+    @staticmethod
+    def int_to_alphabetic(integer):
+        s = ""
+        while integer != 0:
+            s = chr(ord("A") + integer % 10) + s
+            integer //= 10
+        return s
 
     @staticmethod
     def shortname_for_word(info, word):
-        if len(word) == 0:
+        if not word:
             return ""
-        short_word = None
         if any(char.isdigit() for char in word):
             raise Exception(f"Parameters should not contain numbers: '{word}' contains a number")
-        if word in info["short_word"]:
-            return info["short_word"][word]
+        if (short_word := info["short_word"].get(word)):
+            return short_word
         for prefix_len in range(1, len(word) + 1):
             prefix = word[:prefix_len]
             if prefix in info["reverse_short_word"]:
                 continue
-            else:
-                short_word = prefix
-                break
-
-        if short_word is None:
-            # Paranoid fallback
-            def int_to_alphabetic(integer):
-                s = ""
-                while integer != 0:
-                    s = chr(ord("A") + integer % 10) + s
-                    integer //= 10
-                return s
-
+            short_word = prefix
+            break
+        else:
             i = 0
             while True:
-                sword = word + "#" + int_to_alphabetic(i)
+                sword = word + "#" + TrialShortNamer.int_to_alphabetic(i)
                 if sword in info["reverse_short_word"]:
                     continue
-                else:
-                    short_word = sword
-                    break
-
+                short_word = sword
+                break
         info["short_word"][word] = short_word
         info["reverse_short_word"][short_word] = word
         return short_word
@@ -69,20 +47,13 @@ class TrialShortNamer:
     @staticmethod
     def shortname_for_key(info, param_name):
         words = param_name.split("_")
-
         shortname_parts = [TrialShortNamer.shortname_for_word(info, word) for word in words]
-
-        # We try to create a separatorless short name, but if there is a collision we have to fallback
-        # to a separated short name
-        separators = ["", "_"]
-
-        for separator in separators:
+        for separator in ("", "_"):
             shortname = separator.join(shortname_parts)
             if shortname not in info["reverse_short_param"]:
                 info["short_param"][param_name] = shortname
                 info["reverse_short_param"][shortname] = param_name
                 return shortname
-
         return param_name
 
     @staticmethod
@@ -93,70 +64,45 @@ class TrialShortNamer:
 
     @classmethod
     def build_naming_info(cls):
-        if cls.NAMING_INFO is not None:
-            return
-
-        info = {
-            "short_word": {},
-            "reverse_short_word": {},
-            "short_param": {},
-            "reverse_short_param": {},
-        }
-
-        field_keys = list(cls.DEFAULTS.keys())
-
-        for k in field_keys:
-            cls.add_new_param_name(info, k)
-
-        cls.NAMING_INFO = info
+        if cls.DEFAULTS:
+            cls.NAMING_INFO = {
+                "short_word": {},
+                "reverse_short_word": {},
+                "short_param": {},
+                "reverse_short_param": {},
+            }
+            for k in cls.DEFAULTS:
+                cls.add_new_param_name(cls.NAMING_INFO, k)
 
     @classmethod
     def shortname(cls, params):
-        cls.build_naming_info()
         assert cls.PREFIX is not None
         name = [copy.copy(cls.PREFIX)]
-
         for k, v in params.items():
             if k not in cls.DEFAULTS:
                 raise Exception(f"You should provide a default value for the param name {k} with value {v}")
             if v == cls.DEFAULTS[k]:
-                # The default value is not added to the name
                 continue
-
             key = cls.NAMING_INFO["short_param"][k]
-
-            if isinstance(v, bool):
-                v = 1 if v else 0
-
             sep = "" if isinstance(v, (int, float)) else "-"
-            e = f"{key}{sep}{v}"
-            name.append(e)
-
+            name.append(f"{key}{sep}{v}")
         return "_".join(name)
 
     @classmethod
     def parse_repr(cls, repr):
-        repr = repr[len(cls.PREFIX) + 1 :]
-        if repr == "":
-            values = []
-        else:
-            values = repr.split("_")
-
+        repr = repr[len(cls.PREFIX)+1:]
+        values = repr.split("_") if repr else []
         parameters = {}
-
         for value in values:
             if "-" in value:
                 p_k, p_v = value.split("-")
+                p_v = float(re.sub("[^0-9.]", "", p_v))
             else:
                 p_k = re.sub("[0-9.]", "", value)
                 p_v = float(re.sub("[^0-9.]", "", value))
-
             key = cls.NAMING_INFO["reverse_short_param"][p_k]
-
             parameters[key] = p_v
-
         for k in cls.DEFAULTS:
             if k not in parameters:
                 parameters[k] = cls.DEFAULTS[k]
-
         return parameters
