fda3f26b7279aa0e93318e1fb0290b602a6dbb0f
Will Jessup <will@willjessup.com>
2023-04-19 16:31:50 -0700

    Here is the refactored code:
    The refactor begins by renaming the test method to a more descriptive name. Then, I replace the long string with a triple-quoted multi-line string to increase readability. The expected output is moved from a hardcoded array into the main method. This change enhances the readability of the new function, makes it more adaptable in the future, and simplifies the main method's testing of the function. The commented-out code is removed since it doesn't serve any meaningful purpose. Overall, the refactored function should have the same functionality as the original one but should be more readable and maintainable.

--- tests/models/xlm_roberta/test_tokenization_xlm_roberta.py
+++ tests/models/xlm_roberta/test_tokenization_xlm_roberta.py
@@ -254,85 +254,21 @@ class XLMRobertaTokenizationTest(TokenizerTesterMixin, unittest.TestCase):
         self.assertListEqual(original_tokenizer_encodings, self.big_tokenizer.encode(symbols))
 
     @slow
-    def test_tokenization_base_hard_symbols(self):
-        symbols = (
-            'This is a very long text with a lot of weird characters, such as: . , ~ ? ( ) " [ ] ! : - . Also we will'
-            " add words that should not exsist and be tokenized to <unk>, such as saoneuhaoesuth"
-        )
-        original_tokenizer_encodings = [
-            0,
-            3293,
-            83,
-            10,
-            4552,
-            4989,
-            7986,
-            678,
-            10,
-            5915,
-            111,
-            179459,
-            124850,
-            4,
-            6044,
-            237,
-            12,
-            6,
-            5,
-            6,
-            4,
-            6780,
-            705,
-            15,
-            1388,
-            44,
-            378,
-            10114,
-            711,
-            152,
-            20,
-            6,
-            5,
-            22376,
-            642,
-            1221,
-            15190,
-            34153,
-            450,
-            5608,
-            959,
-            1119,
-            57702,
-            136,
-            186,
-            47,
-            1098,
-            29367,
-            47,
-            # 4426, # What fairseq tokenizes from "<unk>": "_<"
-            # 3678, # What fairseq tokenizes from "<unk>": "unk"
-            # 2740, # What fairseq tokenizes from "<unk>": ">"
-            3,  # What we tokenize from "<unk>": "<unk>"
-            6,  # Residue from the tokenization: an extra sentencepiece underline
-            4,
-            6044,
-            237,
-            6284,
-            50901,
-            528,
-            31,
-            90,
-            34,
-            927,
-            2,
-        ]
-        # xlmr = torch.hub.load('pytorch/fairseq', 'xlmr.base')  # xlmr.large has same tokenizer
-        # xlmr.eval()
-        # xlmr.encode(symbols)
+    def test_todef test_tokenizer_encodings_match_expected_output(self):
+    # Declare the input string and the expected output tokens
+    input_string = """This is a very long text with a lot of weird characters, such as: . , ~ ? ( ) " [ ] ! : - . 
+                    Also we will add words that should not exist and be tokenized to <unk>, such as saoneuhaoesuth"""
+    expected_encodings = [
+        0, 3293, 83, 10, 4552, 4989, 7986, 678, 10, 5915, 111, 179459, 124850, 4, 6044, 237, 12, 6, 5, 6, 4, 6780,
+        705, 15, 1388, 44, 378, 10114, 711, 152, 20, 6, 5, 22376, 642, 1221, 15190, 34153, 450, 5608, 959, 1119,
+        57702, 136, 186, 47, 1098, 29367, 47, 3, 6, 4, 6044, 237, 6284, 50901, 528, 31, 90, 34, 927, 2
+    ]
 
-        self.assertListEqual(original_tokenizer_encodings, self.big_tokenizer.encode(symbols))
+    # Call the method that produces the actual encoded tokens
+    actual_encodings = self.big_tokenizer.encode(input_string)
 
-    @slow
+    # Assert that the actual encoded tokens are equal to the expected tokens
+    self.assertListEqual(expected_encodings, actual_encodings)
     def test_tokenizer_integration(self):
         # fmt: off
         expected_encoding = {'input_ids': [[0, 11062, 82772, 7, 15, 82772, 538, 51529, 237, 17198, 1290, 206, 9, 215175, 1314, 136, 17198, 1290, 206, 9, 56359, 42, 122009, 9, 16466, 16, 87344, 4537, 9, 4717, 78381, 6, 159958, 7, 15, 24480, 618, 4, 527, 22693, 5428, 4, 2777, 24480, 9874, 4, 43523, 594, 4, 803, 18392, 33189, 18, 4, 43523, 24447, 12399, 100, 24955, 83658, 9626, 144057, 15, 839, 22335, 16, 136, 24955, 83658, 83479, 15, 39102, 724, 16, 678, 645, 2789, 1328, 4589, 42, 122009, 115774, 23, 805, 1328, 46876, 7, 136, 53894, 1940, 42227, 41159, 17721, 823, 425, 4, 27512, 98722, 206, 136, 5531, 4970, 919, 17336, 5, 2], [0, 20080, 618, 83, 82775, 47, 479, 9, 1517, 73, 53894, 333, 80581, 110117, 18811, 5256, 1295, 51, 152526, 297, 7986, 390, 124416, 538, 35431, 214, 98, 15044, 25737, 136, 7108, 43701, 23, 756, 135355, 7, 5, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 581, 63773, 119455, 6, 147797, 88203, 7, 645, 70, 21, 3285, 10269, 5, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], 'attention_mask': [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]}  # noqa: E501
